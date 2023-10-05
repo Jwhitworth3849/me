3cd0d318d03697421d96bd5e17d3a7f6ef13eb35
Jwhitworth3849 <88682686+Jwhitworth3849@users.noreply.github.com>
2023-10-05 12:11:35 -0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function item4function() {
+    document.getElementById("aboutME").scrollIntoView({behavior: 'smooth'});
+}
+function myFunction() {
+    document.getElementById("title").scrollIntoView({behavior: 'smooth'});
+}
